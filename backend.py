ccf460e4faef756a6b0f9688ea176664592678de
Yuliya <123246886+Yuliya3003@users.noreply.github.com>
2024-12-13 21:04:37 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import json
 import uvicorn
 import yape
 from transformers import AutoTokenizer, AutoModel
@@ -7,114 +8,133 @@ from pydantic import BaseModel
 from weaviate.client_base import ConnectionParams
 from weaviate import WeaviateClient
 from weaviate.classes.config import Property, DataType
-import weaviate
+from weaviate.classes.config import Configure
+
 
 class Document(BaseModel):
     content: str
     dataframe: str = None
     keywords: list[str] = []
 
-# FastAPI приложение
+
 app = FastAPI()
 
 # Создание параметров подключения
 connection_params = ConnectionParams(
     http={
         "host": "localhost",  # Хост Weaviate-сервера
-        "port": 8080,         # Порт Weaviate-сервера
-        "secure": False,      # Использовать ли HTTPS (False - HTTP)
+        "port": 8080,  # Порт Weaviate-сервера
+        "secure": False,  # Использовать ли HTTPS (False - HTTP)
     },
     grpc={
         "host": "localhost",  # Хост для gRPC (если не используете gRPC, все равно указывайте)
-        "port": 50051,        # Порт gRPC
-        "secure": False       # Без шифрования для gRPC
+        "port": 50051,  # Порт gRPC
+        "secure": False  # Без шифрования для gRPC
     }
 )
 
-# Инициализация клиента
 client = WeaviateClient(connection_params=connection_params)
-client.connect()
 
-if client.is_ready():
-    print("Weaviate server is ready!")
-else:
-    print("Failed to connect to Weaviate server.")
+
+def ensure_client_connected():
+    if not client.is_connected():
+        print("Client is not connected. Connecting to Weaviate...")
+        client.connect()
+        print("Weaviate is connected")
+
+
+def create_test_article_collection():
+    try:
+        client.collections.create(
+            "Paragraphs",
+            vectorizer_config=Configure.Vectorizer.text2vec_huggingface(),
+            vector_index_config=Configure.VectorIndex.hnsw(),
+            properties=[
+                Property(name="content", data_type=DataType.TEXT),
+                Property(name="dataframe", data_type=DataType.TEXT),
+                Property(name="keywords", data_type=DataType.TEXT_ARRAY),
+            ]
+        )
+        print("Коллекция 'Paragraphs' создана.")
+    except Exception as e:
+        print(f"Error creating collection: {e}")
+
+
+# create_test_article_collection()
 
 # Модель E5 для генерации эмбеддингов
 model_name = "intfloat/e5-large-v2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
-# Функция для векторизации текста
+
 def embed_text(text):
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
     with torch.no_grad():
         embeddings = model(**inputs).last_hidden_state.mean(dim=1)
     return embeddings.squeeze().numpy()
 
+
 # Функция для извлечения ключевых слов с использованием Yape
-def extract_keywords(text):
-    extractor = yape.KeywordExtractor()
-    keywords = extractor.extract_keywords(text)
-    return [keyword for keyword, score in keywords]
+# def extract_keywords(text):
+#     extractor = yape.KeywordExtractor()
+#     keywords = extractor.extract_keywords(text)
+#     return [keyword for keyword, score in keywords]
 
 
-def create_test_article_collection():
-    # Создание новой коллекции (класса)
-    collection_name = "TestArticle"  # имя класса (коллекции)
-    properties = [
-        Property(name="content", data_type=DataType.TEXT),  # Corrected key name
-        Property(name="dataframe", data_type=DataType.TEXT),
-        Property(name="keywords", data_type=DataType.TEXT_ARRAY),  # Use TEXT_ARRAY for array type
-    ]
-
-    # Создание коллекции (класса) через Weaviate client
-    client.collections.create(
-        name=collection_name,
-        properties=properties
-    )
-    print("Коллекция 'TestArticle' создана.")
-
-#create_test_article_collection()
-
-# Индексация документов с использованием векторизации
 @app.post("/indexing")
 async def index_docs_with_embeddings(docs: list[Document]):
     try:
+        ensure_client_connected()
         for doc in docs:
-            embedding = embed_text(doc.content)  # Vectorization
-            response = client.data_object.create(
-                data_object={
-                    "content": embedding,
-                    "dataframe": doc.dataframe,
-                    "keywords": doc.keywords,
-                },
-                class_name="TestArticle",
-                vector=embedding.tolist()  # Ensure the vector is a list
-            )
+            embedding = embed_text(doc.content)
+            with client.batch.dynamic() as batch:
+                batch.add_object(
+                    properties={
+                        "content": doc.content,
+                        "dataframe": doc.dataframe,
+                        "keywords": doc.keywords,
+                    },
+                    vector=embedding.tolist(),
+                    collection="Paragraphs"
+                )
         return {"message": "Documents indexed with embeddings"}
     except Exception as e:
-        return {"error": str(e)}
+        print(f"Failed to add document: {e}")
+        if client.batch.failed_objects:
+            print("Failed objects:", client.batch.failed_objects)
+        return {"error": f"Document failed to index: {e}"}
+
+
+class SearchQuery(BaseModel):
+    text: str
+    top_k: int
+
 
 @app.post("/searching")
-async def search_with_llm(text: str, top_k: int = 3):
-    # Извлекаем ключевые слова из запроса
-    query_keywords = extract_keywords(text)
+async def search_with_llm(query: SearchQuery):
+    text = query.text
+    top_k = query.top_k
 
-    query_embedding = embed_text(text)  # Векторизуем запрос
+    query_embedding = embed_text(text)
     near_vector = {"vector": query_embedding.tolist()}
 
-    # Выполняем запрос к Weaviate с фильтрацией по ключевым словам
-    result = ((((client.query.get("Paragraph", ["content", "dataframe", "keywords"])
-              .with_near_vector(near_vector))
-              .with_limit(top_k))
-              .with_where({
-                    "operator": "Like",
-                    "path": ["keywords"],
-                    "valueString": query_keywords[0]  # Пример фильтрации по первому ключевому слову
-              }))
-              .do())
-    return {"results": result["data"]["Get"]["Paragraph"]}
+    paragraphs = client.collections.get("Paragraphs")
+    result = paragraphs.query.near_vector(
+        query_embedding,
+        limit=top_k
+    )
+    res = ""
+    for obj in result.objects:
+        res += json.dumps(obj.properties, indent=2)
+    return res
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    client.close()
+    print("Weaviate client closed.")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
